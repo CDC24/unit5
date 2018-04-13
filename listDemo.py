3674d841d75b2a3fb4424ddd7e4874b88bc51935
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-13 08:34:45 -0400

    Updated from Brython Server: 13/04/2018 08:34:44

--- listDemo.py
+++ listDemo.py
@@ -6,5 +6,11 @@
 
 words = input("Enter some words please: ").split(" ")
 
-print("The first word was",words[o])
+print("The first word was",words[0])
 print("The last word was",words[-1])
+
+
+#print list one item at a time
+
+for item in words:
+    print (item)
