5327a3f67242b9c71d70dfc761a521593682df5e
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-13 08:48:12 -0400

    Updated from Brython Server: 13/04/2018 08:48:10

--- wordSort.py
+++ wordSort.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #4/13/18
-#wordSort.py
+#wordSort.py - print out list alphabetically
+
+
+words = input("Enter some words please: ").split(" ")
+
+sorted = words.sort()
+
+for item in sorted:
+    print (item)
