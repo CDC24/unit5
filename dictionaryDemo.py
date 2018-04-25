df642a818e7f728eef36ebf9f633f98572853151
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-25 08:10:12 -0400

    Updated from Brython Server: 25/04/2018 08:10:11

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -7,6 +7,7 @@ words = ["computer", "mortify", "dog", "firetruck", "yes", "python", "cat"]
 words.sort()
 
 num = int(input("what number word do you want?  "))
-
+if num<=0 or num >=len(words)+1:
+    print("you can't have that")
 
 print(words[num-1])
