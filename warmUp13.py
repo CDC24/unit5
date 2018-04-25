72b64afe9fd4a94cc504883d77b87e277aee261d
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-25 08:15:41 -0400

    Updated from Brython Server: 25/04/2018 08:15:40

--- warmUp13.py
+++ warmUp13.py
@@ -1,3 +1,10 @@
 #Caleb Callaway
 #3/25/18
 #warmUp13.py - list 20 random numbers, prints sum, prints minimum, prints max
+
+from random import randint
+
+nums = []
+
+for i in range (0,21):
+    nums.append(randint)
