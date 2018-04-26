aec37d6b136b37addac5f310554a8c9f96ea97b7
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-26 08:39:49 -0400

    Create longestWord.py

---
+++ longestWord.py
@@ -0,0 +1,14 @@
+#Caleb Callaway
+#3/25/18
+#longestWord.py - prints the longest word in a list
+
+
+w = input("enter some words: ")
+
+
+words = w.split(" ")
+
+if len(words)%2==0:
+    print(words[len(words)/2],words[(len(words)/2)-1])
+else:
+    print (words[len(words)/2])    
