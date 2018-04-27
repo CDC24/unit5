64a03a4d743fa1ba6d3a479d165435d0f17719be
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-27 08:49:28 -0400

    Updated from Brython Server: 27/04/2018 08:49:27

--- displayDate.py
+++ displayDate.py
@@ -1,3 +1,15 @@
 #Caleb Callaway
 #3/27/18
 #displayDate.py - prints out the date
+
+
+from datetime import date
+from calendar import weekday
+
+
+day = date.today().day
+month = date.today().month
+year = date.today().year
+
+
+print (weekday[w])
