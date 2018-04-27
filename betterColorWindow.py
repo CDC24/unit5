3b1794d8c6bd0a3861beca8d0c9ebd89d9cd77bf
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-27 08:43:13 -0400

    Updated from Brython Server: 27/04/2018 08:43:12

--- betterColorWindow.py
+++ betterColorWindow.py
@@ -1,3 +1,25 @@
 #Caleb Callaway
 #3/27/18
 #betterColorWindow.py - color change window with lists
+
+
+from ggame import *
+from random import randint
+
+def randcol(event):
+    gnum = randint (1,4)
+    if gnum == 1:
+        col = Color(0x00FF00,1)
+    elif gnum == 2:
+        col = Color(0xFF0000,1)
+    elif gnum == 3:
+        col =  Color(0x0000FF,1)
+    elif gnum == 4:
+        col = Color(0X00000000,1)
+    theRectangle = RectangleAsset(1500,1000,LineStyle(1,col),col)
+    Sprite(theRectangle)
+
+
+
+App().listenMouseEvent("click",randcol)
+App().run()
