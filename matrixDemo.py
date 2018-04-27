c3ac8c6fbd2a929aa6f31b1c40f61bfd46dae400
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-27 08:28:17 -0400

    Updated from Brython Server: 27/04/2018 08:28:16

--- matrixDemo.py
+++ matrixDemo.py
@@ -4,7 +4,8 @@
 
 def printBoard(board):
     for row in range(0,3):
-        print (board[row])
+        for col in range (0,3):
+            print (board[row])
 
 
 board = [["a","b","c"],["d","e","f"],["g","h","i"]]
