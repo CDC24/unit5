e6ee620b1fa33958a31ecd4d7d3d887655204e93
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-30 08:21:18 -0400

    Updated from Brython Server: 30/04/2018 08:21:17

--- antsDemo.py
+++ antsDemo.py
@@ -5,26 +5,27 @@
 from ggame import *
 from random import randint
 
-ANTS = 50
+ANTS = 100
 WIDTH = 1000
 HEIGHT= 500
 
 
-def step():
+def step():                             #move each ant randomly in x and y
     for ant in data["antlist"]:
-        ant.x += randint (-10,10)
-        ant.y += randint (-10,10)
+        ant.x += randint (-8,10)
+        ant.y += randint (-8,10)
 
-if __name__ == "__main__":
+if __name__ == "__main__":             #putting ants on screen
     
     
     data = {}
-    data["antlist"] = []
+    data["antlist"] = []                #list of ants
     
     red = Color(0xFF0000,1)
     ant = CircleAsset(5,LineStyle(1,red),red)
     
     for i in range (ANTS):
-        data["antlist"].append(Sprite (ant,(randint(1,WIDTH),randint(1,HEIGHT))))
+        #data["antlist"].append(Sprite (ant,(randint(1,WIDTH),randint(1,HEIGHT))))
+        data["antlist"].append(Sprite (ant))
     
     App().run(step)
