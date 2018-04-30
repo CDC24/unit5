bbff6491fc09645f993bec6436f45455d6eccbf5
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-30 08:28:08 -0400

    Updated from Brython Server: 30/04/2018 08:28:08

--- warmUp14.py
+++ warmUp14.py
@@ -4,6 +4,6 @@
 
 
 
-names = input ("What is your full name? ").split(" ")
+names = input ("Enter your first and last name: ").split(" ")
 
-print ("Your initials are", names[0][0], names[1][0], names[2][0], names[3][0], names[4][0], names[5][0])
+print ("Your initials are", names[0][0],".", names[1][0],".")
