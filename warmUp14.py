5dc192ac1038316eeb4ce6b1649a788db7f73a03
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-30 08:23:26 -0400

    Updated from Brython Server: 30/04/2018 08:23:25

--- warmUp14.py
+++ warmUp14.py
@@ -1,3 +1,10 @@
 #Caleb Callaway
 #4/10/18
 # warmUp14.py - ask user for first and last name and tells their initials
+
+
+
+names = input ("What is your full name?").split(" ")
+
+for name in names:
+    print name[0]
