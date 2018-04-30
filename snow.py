19eed97f72cac18d38619aedb58f772fd4ed73e7
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-30 08:41:11 -0400

    Updated from Brython Server: 30/04/2018 08:41:11

--- snow.py
+++ snow.py
@@ -1,3 +1,32 @@
 #Caleb Callaway
 #3/30/18
 #snow.py - pleasant snow
+
+
+from ggame import *
+from random import randint
+
+FLAKES = 100
+WIDTH = 1000
+HEIGHT= 500
+
+
+def step():                             #move each flake randomly in x and y
+    for flake in data["flakelist"]:
+        flake.x += randint (-2,2)
+        flake.y += randint (0,3)
+
+if __name__ == "__main__":             #putting flakes on screen
+    
+    
+    data = {}
+    data["flakelist"] = []                #list of flakes
+    
+    white = Color(0x000000,1)
+    flake = CircleAsset(5,LineStyle(1,white),white)
+    
+    for i in range (FLAKES):
+        data["flakelist"].append(Sprite (flake,(randint(1,WIDTH),randint(1,HEIGHT))))
+    
+    
+    App().run(step)
