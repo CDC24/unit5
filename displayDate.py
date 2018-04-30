82889a035cbfb0b7a6bfc63a3303ace233b2a115
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-30 08:34:19 -0400

    Updated from Brython Server: 30/04/2018 08:34:19

--- displayDate.py
+++ displayDate.py
@@ -12,6 +12,8 @@ month = date.today().month
 year = date.today().year
 
 weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
-print ("Today is",weekdays[weekday(year,month,day)], month)
+
+print ("Today is",weekdays[weekday(year,month,day)],",", months[month-1], day)
 
