17923ec1a23431a43383f76e04ffe7cd4587218a
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-30 08:06:57 -0400

    Updated from Brython Server: 30/04/2018 08:06:57

--- antsDemo.py
+++ antsDemo.py
@@ -1,3 +1,17 @@
 #Caleb Callaway
 #4/10/18
 # antsDemo.py - lists with graphics
+
+from ggame import *
+
+ANTS = 10
+
+if __name__ == "__main__":
+    
+    red = Color(0xFF0000,1)
+    ant = CircleAsset(15,LineStyle(1,red),red)
+    
+    
+    Sprite (ant)
+    
+App().run()
